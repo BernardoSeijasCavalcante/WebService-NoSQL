e0a962e61a54aa27f20de2541fc143b2b2dfd4fd
BernardoSeijasCavalcante <bernardomseijas@gmail.com>
2025-07-16 13:46:30 -0300

    feat: Adiciona entidade Post

--- workshopmongodb/src/main/java/com/multilaseu/workshopmongodb/config/Instantiation.java
+++ workshopmongodb/src/main/java/com/multilaseu/workshopmongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.multilaseu.workshopmongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.multilaseu.workshopmongodb.domain.Post;
 import com.multilaseu.workshopmongodb.domain.User;
+import com.multilaseu.workshopmongodb.repository.PostRepository;
 import com.multilaseu.workshopmongodb.repository.UserRepository;
 
 @Configuration	
@@ -14,6 +18,9 @@ public class Instantiation implements CommandLineRunner{
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,7 +30,18 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		postRepository.deleteAll();
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
 	}
 
 }
---
+++ workshopmongodb/src/main/java/com/multilaseu/workshopmongodb/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.multilaseu.workshopmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.multilaseu.workshopmongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
